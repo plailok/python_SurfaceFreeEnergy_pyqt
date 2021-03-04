fb6e8dc00d5e738793f366551812a3041e2f1571
X-under <frozon201@gmail.com>
2021-03-04 23:16:17 +0100

    add first widget

--- MainWindow_v2.py
+++ MainWindow_v2.py
@@ -2,6 +2,9 @@ from PyQt5 import QtGui, QtCore, QtWidgets
 from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QTableWidgetItem
 from PyQt5.QtCore import pyqtSlot, Qt, QRegExp
 from PyQt5.QtGui import QPixmap, QImage, QRegExpValidator
+from PyQt5.QtWebEngineWidgets import QWebEngineView as QWebView
+from PyQt5.QtWebEngineWidgets import QWebEnginePage as QWebPage
+from PyQt5.QtWebEngineWidgets import QWebEngineSettings as QWebSettings
 from v2_mainwindow import Ui_MainWindow
 from SDF_dialog import Ui_Dialog
 from main_calculation import Calculation
@@ -14,18 +17,38 @@ class MyWindow_v2(QtWidgets.QMainWindow):
         super(MyWindow_v2, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.__setupUI()
+        self.web = QWebView(self.ui.theorysFrame)
+        self.web.setHtml('/Refractive index - Wikipedia.html')
 
     def __setupUI(self):
-        pass
+        self.ui.theoryButton.setEnabled(False)
+        self.ui.theoryButton.clicked.connect(self.__theory_button_checked)
+        self.ui.settingsButton.clicked.connect(self.__settings_button_checked)
+        self.ui.measurmentButton.clicked.connect(self.__measurment_buttton_checked)
 
     def __theory_button_checked(self):
-        pass
+        print('Theory button checked')
+        self.ui.theoryButton.setEnabled(False)
+        self.ui.settingsButton.setEnabled(True)
+        self.ui.measurmentButton.setEnabled(True)
 
     def __settings_button_checked(self):
-        pass
+        print('Settings button checked')
+        self.ui.theoryButton.setEnabled(True)
+        self.ui.settingsButton.setEnabled(False)
+        self.ui.measurmentButton.setEnabled(True)
 
-    def __result_buttton_checked(self):
-        pass
+    def __measurment_buttton_checked(self):
+        print('Measurment button checked')
+        self.ui.theoryButton.setEnabled(True)
+        self.ui.settingsButton.setEnabled(True)
+        self.ui.measurmentButton.setEnabled(False)
+
+    def __theory_chose(self):
+        print('Theory had been changed')
+        print(f'Current theory - {self.ui.theoryCombobox.currentText()}')
+
 
 if __name__ == '__main__':
     try:
--- probe.py
+++ probe.py
@@ -13,72 +13,72 @@ from optparse import OptionParser
 
 #
 #
-# class MyBrowser(QWebPage):
-#     ''' Settings for the browser.'''
-#
-#     def userAgentForUrl(self, url):
-#         ''' Returns a User Agent that will be seen by the website. '''
-#         return "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
-#
-#
-# class Browser(QWebView):
-#     def __init__(self):
-#         # QWebView
-#         self.view = QWebView.__init__(self)
-#         # self.view.setPage(MyBrowser())
-#         self.setWindowTitle('Loading...')
-#         self.titleChanged.connect(self.adjustTitle)
-#         # super(Browser).connect(self.ui.webView,QtCore.SIGNAL("titleChanged (const QString&amp;)"), self.adjustTitle)
-#
-#     def load(self, url):
-#         self.setUrl(QUrl(url))
-#
-#     def adjustTitle(self):
-#         self.setWindowTitle(self.title())
-#
-#     def disableJS(self):
-#         settings = QWebSettings.globalSettings()
-#         settings.setAttribute(QWebSettings.JavascriptEnabled, False)
-#
-#
-# app = QApplication(sys.argv)
-# view = Browser()
-# view.showMaximized()
-# view.load("https://www.kruss-scientific.com/en/know-how/glossary/oss-and-good-method")
-# app.exec_()
+class MyBrowser(QWebPage):
+    ''' Settings for the browser.'''
 
-class Example(QWidget):
+    def userAgentForUrl(self, url):
+        ''' Returns a User Agent that will be seen by the website. '''
+        return "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
 
+
+class Browser(QWebView):
     def __init__(self):
-        super().__init__()
+        # QWebView
+        self.view = QWebView.__init__(self)
+        # self.view.setPage(MyBrowser())
+        self.setWindowTitle('Loading...')
+        self.titleChanged.connect(self.adjustTitle)
+        # super(Browser).connect(self.ui.webView,QtCore.SIGNAL("titleChanged (const QString&amp;)"), self.adjustTitle)
 
-        self.initUI()
+    def load(self, url):
+        self.setUrl(QUrl(url))
 
-    def initUI(self):
-        vbox = QVBoxLayout(self)
+    def adjustTitle(self):
+        self.setWindowTitle(self.title())
 
-        self.webEngineView = QWebEngineView()
-        self.loadPage()
-
-        vbox.addWidget(self.webEngineView)
-
-        self.setLayout(vbox)
-
-        self.setGeometry(300, 300, 350, 250)
-        self.setWindowTitle('QWebEngineView')
-        self.show()
-
-    def loadPage(self):
-        with open('theories/Refractive index - Wikipedia.html', 'r') as f:
-            html = f.read()
-            self.webEngineView.setHtml(html)
+    def disableJS(self):
+        settings = QWebSettings.globalSettings()
+        settings.setAttribute(QWebSettings.JavascriptEnabled, False)
 
 
-def main():
-    app = QApplication(sys.argv)
-    ex = Example()
-    sys.exit(app.exec_())
-
-
-if __name__ == '__main__':
-    main()
+app = QApplication(sys.argv)
+view = Browser()
+view.showMaximized()
+view.load('/theories/Refractive index - Wikipedia.html')
+app.exec_()
+#
+# class Example(QWidget):
+#
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.initUI()
+#
+#     def initUI(self):
+#         vbox = QVBoxLayout(self)
+#
+#         self.webEngineView = QWebEngineView()
+#         self.loadPage()
+#
+#         vbox.addWidget(self.webEngineView)
+#
+#         self.setLayout(vbox)
+#
+#         self.setGeometry(300, 300, 350, 250)
+#         self.setWindowTitle('QWebEngineView')
+#         self.show()
+#
+#     def loadPage(self):
+#         with open('theories/Refractive index - Wikipedia.html', 'r') as f:
+#             html = f.read()
+#             self.webEngineView.setHtml(html)
+#
+#
+# def main():
+#     app = QApplication(sys.argv)
+#     ex = Example()
+#     sys.exit(app.exec_())
+#
+#
+# if __name__ == '__main__':
+#     main()
