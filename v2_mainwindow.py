c0609fb320a4bd8f39d4dfdcdc7b8ea3bf9015ff
X-under <frozon201@gmail.com>
2021-03-07 22:08:46 +0100

    add setting table

--- v2_mainwindow.py
+++ v2_mainwindow.py
@@ -14,14 +14,15 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1246, 608)
+        MainWindow.resize(1246, 564)
         MainWindow.setStyleSheet("QFrame{\n"
-"    background-color: rgb(120, 120, 120);\n"
-"    border-radius: 25px;\n"
-"}\n"
-"QMainWindow{\n"
-"    background-color:rgb(255,255,255)\n"
-"}")
+                                 "    background-color: rgb(120, 120, 120);\n"
+                                 "    border-radius: 25px;\n"
+                                 "}\n"
+                                 "QMainWindow{\n"
+                                 "    background-color:rgb(255,255,255)\n"
+                                 "}\n"
+                                 "")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout_6 = QtWidgets.QGridLayout(self.centralwidget)
@@ -30,27 +31,27 @@ class Ui_MainWindow(object):
         self.gridLayout_2.setObjectName("gridLayout_2")
         self.ButtonsFrame = QtWidgets.QFrame(self.centralwidget)
         self.ButtonsFrame.setMinimumSize(QtCore.QSize(140, 480))
-        self.ButtonsFrame.setMaximumSize(QtCore.QSize(200, 16377))
+        self.ButtonsFrame.setMaximumSize(QtCore.QSize(200, 480))
         self.ButtonsFrame.setStyleSheet("QPushButton {\n"
-"    color: rgb(250, 120, 255);\n"
-"    background-color: grey;\n"
-"    border-style: outset;\n"
-"    border-width: 3px;\n"
-"    border-radius: 25px;\n"
-"    border-color: rgb(255,120,200);\n"
-"    font: bold 14px;\n"
-"    min-width: 140px;\n"
-"    padding: 6px;\n"
-"}\n"
-"QPushButton:checked {\n"
-"    background-color: rgb(80, 80, 80);\n"
-"    border-style: inset;\n"
-"}\n"
-"QFrame{\n"
-"    background-color: rgb(120, 120, 120);\n"
-"    border-radius: 25px;\n"
-"}\n"
-"")
+                                        "    color: rgb(250, 120, 255);\n"
+                                        "    background-color: grey;\n"
+                                        "    border-style: outset;\n"
+                                        "    border-width: 3px;\n"
+                                        "    border-radius: 25px;\n"
+                                        "    border-color: rgb(255,120,200);\n"
+                                        "    font: bold 14px;\n"
+                                        "    min-width: 140px;\n"
+                                        "    padding: 6px;\n"
+                                        "}\n"
+                                        "QPushButton:checked {\n"
+                                        "    background-color: rgb(80, 80, 80);\n"
+                                        "    border-style: inset;\n"
+                                        "}\n"
+                                        "QFrame{\n"
+                                        "    background-color: rgb(120, 120, 120);\n"
+                                        "    border-radius: 25px;\n"
+                                        "}\n"
+                                        "")
         self.ButtonsFrame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.ButtonsFrame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.ButtonsFrame.setObjectName("ButtonsFrame")
@@ -87,19 +88,20 @@ class Ui_MainWindow(object):
         self.gridLayout_2.addWidget(self.ButtonsFrame, 0, 0, 1, 1)
         self.MainFrame = QtWidgets.QFrame(self.centralwidget)
         self.MainFrame.setMinimumSize(QtCore.QSize(700, 480))
+        self.MainFrame.setMaximumSize(QtCore.QSize(16777215, 480))
         self.MainFrame.setToolTipDuration(1)
         self.MainFrame.setStyleSheet("QFrame{\n"
-"    backgroundcolor: rgb(255, 255, 0)}\n"
-"QComboBox {\n"
-"    border: 1px solid gray;\n"
-"    border-radius: 10px;\n"
-"    padding: 5px 2px 5px 2px;\n"
-"    min-width: 300px;\n"
-"    font: bold, 14px;\n"
-"}\n"
-"QComboBox::drop-down {\n"
-"    border: 0px;\n"
-"}")
+                                     "    backgroundcolor: rgb(255, 255, 0)}\n"
+                                     "QComboBox {\n"
+                                     "    border: 1px solid gray;\n"
+                                     "    border-radius: 10px;\n"
+                                     "    padding: 5px 2px 5px 2px;\n"
+                                     "    min-width: 300px;\n"
+                                     "    font: bold, 14px;\n"
+                                     "}\n"
+                                     "QComboBox::drop-down {\n"
+                                     "    border: 0px;\n"
+                                     "}")
         self.MainFrame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.MainFrame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.MainFrame.setObjectName("MainFrame")
@@ -113,13 +115,13 @@ class Ui_MainWindow(object):
         self.gridLayout_4.setObjectName("gridLayout_4")
         self.ctywLabel = QtWidgets.QLabel(self.innerMainFrame)
         self.ctywLabel.setStyleSheet("QLabel{\n"
-"    color:rgb(250, 120, 255);\n"
-"    background-color: rgb(80, 80, 80);\n"
-"    min-width: 220px;\n"
-"    max-width:250px;\n"
-"    border-radius: 10px;\n"
-"    font: bold, 14px\n"
-"}")
+                                     "    color:rgb(250, 120, 255);\n"
+                                     "    background-color: rgb(80, 80, 80);\n"
+                                     "    min-width: 220px;\n"
+                                     "    max-width:250px;\n"
+                                     "    border-radius: 10px;\n"
+                                     "    font: bold, 14px\n"
+                                     "}")
         self.ctywLabel.setScaledContents(True)
         self.ctywLabel.setAlignment(QtCore.Qt.AlignCenter)
         self.ctywLabel.setObjectName("ctywLabel")
@@ -134,13 +136,13 @@ class Ui_MainWindow(object):
         self.theoryCombobox.setLayoutDirection(QtCore.Qt.RightToLeft)
         self.theoryCombobox.setAutoFillBackground(False)
         self.theoryCombobox.setStyleSheet("QFrame{\n"
-"    background-color: rgb(255,255,255);\n"
-"    border-radius: 2px;\n"
-"}\n"
-"QComboBox{\n"
-"    max-width:450px}\n"
-"\n"
-"")
+                                          "    background-color: rgb(255,255,255);\n"
+                                          "    border-radius: 2px;\n"
+                                          "}\n"
+                                          "QComboBox{\n"
+                                          "    max-width:450px}\n"
+                                          "\n"
+                                          "")
         self.theoryCombobox.setInsertPolicy(QtWidgets.QComboBox.InsertAtBottom)
         self.theoryCombobox.setObjectName("theoryCombobox")
         self.theoryCombobox.addItem("")
@@ -149,12 +151,12 @@ class Ui_MainWindow(object):
         self.gridLayout_4.addWidget(self.theoryCombobox, 0, 1, 1, 1)
         self.tempCombobox = QtWidgets.QComboBox(self.innerMainFrame)
         self.tempCombobox.setStyleSheet("QComboBox{\n"
-"    min-width: 50px;\n"
-"    max-width: 100px\n"
-"}\n"
-"QFrame{\n"
-"    background-color: white;\n"
-"}")
+                                        "    min-width: 50px;\n"
+                                        "    max-width: 100px\n"
+                                        "}\n"
+                                        "QFrame{\n"
+                                        "    background-color: white;\n"
+                                        "}")
         self.tempCombobox.setObjectName("tempCombobox")
         self.tempCombobox.addItem("")
         self.tempCombobox.addItem("")
@@ -176,13 +178,13 @@ class Ui_MainWindow(object):
         self.gridLayout_4.addWidget(self.theorysFrame, 1, 0, 1, 4)
         self.tempLabel = QtWidgets.QLabel(self.innerMainFrame)
         self.tempLabel.setStyleSheet("QLabel{\n"
-"    color:rgb(250, 120, 255);\n"
-"    background-color: rgb(80, 80, 80);\n"
-"    min-width: 240px;\n"
-"    max-width:250px;\n"
-"    border-radius: 10px;\n"
-"    font: bold, 14px\n"
-"}")
+                                     "    color:rgb(250, 120, 255);\n"
+                                     "    background-color: rgb(80, 80, 80);\n"
+                                     "    min-width: 240px;\n"
+                                     "    max-width:250px;\n"
+                                     "    border-radius: 10px;\n"
+                                     "    font: bold, 14px\n"
+                                     "}")
         self.tempLabel.setScaledContents(True)
         self.tempLabel.setAlignment(QtCore.Qt.AlignCenter)
         self.tempLabel.setObjectName("tempLabel")
@@ -230,6 +232,7 @@ class Ui_MainWindow(object):
 
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
